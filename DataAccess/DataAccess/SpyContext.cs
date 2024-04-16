839a17b77de21696cc595007b2e0f21ff7beb26f
Yakov Burtik <100514972+xKyPaToPx@users.noreply.github.com>
2024-04-16 18:48:08 +0300

    ver 0.2

---
+++ DataAccess/DataAccess/SpyContext.cs
@@ -0,0 +1,22 @@
+﻿using DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace pisLab1;
+
+public class SpyContext: DbContext
+{
+    public static DbContextOptions<SpyContext> Options { get; set; }
+    public DbSet<Spy> Spies => Set<Spy>();
+    public DbSet<Equipment> Equipments => Set<Equipment>();
+    public DbSet<Mission> Missions => Set<Mission>();
+
+    public SpyContext() : base(Options)
+    {
+        
+    }
+    
+        
+   
+    
+}
---
+++ DataAccess/Models/Spy.cs
@@ -0,0 +1,17 @@
+﻿namespace DataAccess.Models;
+
+public class Spy
+{
+    public Guid Id { get; set; }
+    
+    public string? FirstName { get; set; }
+    
+    public string? SecondName { get; set; }
+    
+    public string? Alias{get;set;}
+    
+    public DateTime? DateOfBirth { get; set; }
+    
+    public string? Nationality { get; set; }
+}
+
